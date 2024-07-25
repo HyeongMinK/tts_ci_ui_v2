2d38e36171a32332cf5b37102b5accc598ec2c72
HyeongMinK <aalynm@gmail.com>
2024-07-25 17:15:58 +0900

    v2

--- app.py
+++ app.py
@@ -376,57 +376,39 @@ if __name__ == '__main__':
 
     if not st.session_state.process_started:
         if st.button("영상 만들기 시작하기"):
-            # Streamlit 애플리케이션 시작 시 체크포인트 다운로드
             download_checkpoint()
-            # 다운로드 버튼이 눌리면 폴더 내의 모든 파일 삭제
             clear_directory("text_files")
             clear_directory("pic_files")
             clear_directory("results")
             clear_directory("audio_files")
             st.session_state.process_started = True
-            st.rerun()
-            
+            st.experimental_rerun()
+
     if st.session_state.process_started:
-        api_key = os.getenv('OPENAI_API_KEY')  # 환경 변수에서 API 키를 가져옵니다.
+        api_key = os.getenv('OPENAI_API_KEY')
         if not api_key:
             raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
 
-        # 텍스트 파일 업로드 위젯 추가
         uploaded_file = st.file_uploader("TTS 생성을 위한 텍스트 파일을 업로드 하세요", type="txt")
 
         if uploaded_file is not None:
-            # 업로드된 파일을 text_files 폴더에 저장
             save_path = os.path.join("text_files", uploaded_file.name)
-            
-            # 디렉토리가 없으면 생성
             if not os.path.exists("text_files"):
                 os.makedirs("text_files")
-
-            # 파일 저장
             with open(save_path, "wb") as f:
                 f.write(uploaded_file.getvalue())
-            
-            # 업로드된 텍스트 파일의 내용을 읽고 화면에 표시
             with open(save_path, "r", encoding="utf-8") as f:
                 file_contents = f.read()
                 st.text_area("업로드된 텍스트 파일 내용", file_contents, height=150)
 
-        # 이미지 파일 업로드 위젯 추가
         uploaded_img_file = st.file_uploader("이미지 파일을 업로드 하세요", type=["jpg", "jpeg", "png"])
 
         if uploaded_img_file is not None:
-            # 업로드된 파일을 pic_files 폴더에 저장
             img_save_path = os.path.join("pic_files", uploaded_img_file.name)
-            
-            # 디렉토리가 없으면 생성
             if not os.path.exists("pic_files"):
                 os.makedirs("pic_files")
-
-            # 파일 저장
             with open(img_save_path, "wb") as f:
                 f.write(uploaded_img_file.getvalue())
-
-            # 업로드된 이미지 파일을 열고 화면에 표시
             img = Image.open(img_save_path)
             st.image(img, caption="업로드된 이미지", width=200)
 
@@ -436,33 +418,27 @@ if __name__ == '__main__':
                 voice_options = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
                 selected_voice = st.radio("Select a voice option for TTS", voice_options, index=0, help="Previews can be found [here](https://platform.openai.com/docs/guides/text-to-speech/voice-options)")
             with col2_file_uploader:
+                st.markdown("**Audio Samples:**")
                 for name, file_path in audio_ex_files.items():
-                    st.subheader(name)
+                    st.write(f"**{name}**")
                     audio_bytes = open(file_path, "rb").read()
                     st.audio(audio_bytes, format='audio/mp3')
-            
 
-            # 선택된 결과를 변수에 저장
             st.session_state.selected_voice = selected_voice
 
-            
-            # Streamlit 버튼을 추가하여 TTS 파일 생성 및 Wav2Lip 실행을 트리거
             if st.button("립싱크 영상 생성하기"):
                 clear_directory("audio_files")
                 clear_directory("results")
                 with st.spinner("TTS 파일 생성 중..."):
-                    create_tts_files(api_key,uploaded_file.name, st.session_state.selected_voice)  # TTS 파일 생성
-
+                    create_tts_files(api_key, uploaded_file.name, st.session_state.selected_voice)
                 with st.spinner("영상 파일 생성 중..."):
-                    result_filename = main(img_save_path)  # Wav2Lip 실행 및 결과 파일 생성
-
-                # 결과 파일에 대해 다운로드 버튼 추가
+                    result_filename = main(img_save_path)
                 if os.path.exists(result_filename):
                     clear_directory("text_files")
                     clear_directory("pic_files")
                     with open(result_filename, "rb") as f:
                         st.success("영상이 성공적으로 생성되었습니다.")
-                        download_button = st.download_button(
+                        st.download_button(
                             label=f"Download {os.path.basename(result_filename)}",
                             data=f,
                             file_name=os.path.basename(result_filename),
